412e883a6272edad937fd8dde214142fa28e4d65
Arth Srivastava <arthsrivastava1@gmail.com>
2026-09-11 03:06:02 +0530

    refactor: move section-header table to end of file, outside PT_LOAD (#2511)

--- libwild/src/elf.rs
+++ libwild/src/elf.rs
@@ -241,6 +241,7 @@ pub(crate) trait ElfClass: Copy + Default + Send + Sync + std::fmt::Debug + 'sta
     const NOTE_HEADER_SIZE: u64 = size_of::<NoteHeader<Self>>() as u64;
     const GNU_HASH_BLOOM_SIZE: u64 = Self::ADDRESS_SIZE;
     const PROGRAM_HEADER_ALIGNMENT: Alignment = Self::ADDRESS_ALIGNMENT;
+    const SECTION_HEADER_ALIGNMENT: Alignment = Self::ADDRESS_ALIGNMENT;
     const GOT_ENTRY_ALIGNMENT: Alignment = Self::ADDRESS_ALIGNMENT;
     const RELA_ENTRY_ALIGNMENT: Alignment = Self::ADDRESS_ALIGNMENT;
     const RELR_ENTRY_ALIGNMENT: Alignment = Self::ADDRESS_ALIGNMENT;
@@ -2507,7 +2508,6 @@ impl<C: ElfClass> platform::Platform for Elf<C> {
 
         builder.add_section(crate::output_section_id::FILE_HEADER);
         builder.add_section(output_section_id::PROGRAM_HEADERS);
-        builder.add_section(output_section_id::SECTION_HEADERS);
         builder.add_section(output_section_id::NOTE_GNU_PROPERTY);
         builder.add_section(output_section_id::NOTE_GNU_BUILD_ID);
         builder.add_section(output_section_id::INTERP);
@@ -2562,6 +2562,7 @@ impl<C: ElfClass> platform::Platform for Elf<C> {
         builder.add_section(output_section_id::SYMTAB_SHNDX_LOCAL);
         builder.add_section(output_section_id::STRTAB);
         builder.add_section(output_section_id::PARTIAL_LINKING_SINGLETONS);
+        builder.add_section(output_section_id::SECTION_HEADERS);
 
         builder.build()
     }
@@ -2773,7 +2774,7 @@ impl<C: ElfClass> platform::Platform for Elf<C> {
                 while let Some((_, seg_id)) = it.next_if(|&(cat, _)| cat == 3) {
                     builder.push_event(OrderEvent::SegmentEnd(seg_id));
                 }
-                builder.push_event(OrderEvent::Section(output_section_id::SECTION_HEADERS));
+
                 for (_, seg_id) in it {
                     builder.push_event(OrderEvent::SegmentStart(seg_id));
                 }
@@ -2837,6 +2838,8 @@ impl<C: ElfClass> platform::Platform for Elf<C> {
         builder.push_event(OrderEvent::SegmentStart(riscv_segment));
         builder.add_section(output_section_id::RISCV_ATTRIBUTES);
         builder.push_event(OrderEvent::SegmentEnd(riscv_segment));
+
+        builder.add_section(output_section_id::SECTION_HEADERS);
 
         Ok(builder.build())
     }
@@ -5518,7 +5521,7 @@ impl<C: ElfClass> Elf<C> {
         };
         defs[output_section_id::SECTION_HEADERS.as_usize()] = BuiltInSectionDetails {
             kind: Self::primary_section(SECTION_HEADERS_SECTION_NAME),
-            section_flags: shf::ALLOC,
+            min_alignment: C::SECTION_HEADER_ALIGNMENT,
             ..Self::DEFAULT_DEFS
         };
         defs[output_section_id::SHSTRTAB.as_usize()] = BuiltInSectionDetails {
